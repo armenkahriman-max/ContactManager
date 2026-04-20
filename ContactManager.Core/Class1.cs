a73e25915042bc3d97cd79d5181a8831e598e04c
armenkahriman-max <armenkahriman93@gmail.com>
2026-04-20 12:21:38 +0200

    1st try dont proabaly not correct

--- ContactManager.Core/Class1.cs
+++ ContactManager.Core/Class1.cs
@@ -1,6 +1,41 @@
-﻿namespace ContactManager.Core;
+﻿using System.Data.Common;
+using System.Dynamic;
 
-public class Class1
+namespace ContactManager.Core;
+
+public class Contact
 {
+    public int Id { get; private set; }
+    public string? Name { get; private set; }
+
+    public Contact(string name,int id)
+    {
+        Name = name;
+        Id = id;
+    }
+    
+
+}
+public class InMemoryContactRepository
+{
+    public void Add(Contact contact) { throw new NotImplementedException(); }
+    public IReadOnlyList<Contact> GetAll() { throw new NotImplementedException(); }
+
+    private List<Contact> contact = new List<Contact>();
+
+    public void AddContact(string name, int id)
+    {
+        if (contact.Any(c => c.Name == name))
+        {
+            throw new ArgumentException("Id exsists");
+        }
+
+        contact.Add(new Contact(name, id));
+
+    }
+    public List<Contact> GetID()
+    {
+        return contact;
+    }
 
 }
--- ContactManager.Tests/UnitTest1.cs
+++ ContactManager.Tests/UnitTest1.cs
@@ -1,10 +1,15 @@
+using ContactManager.Core;
+
 namespace ContactManager.Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void CreateContact_ID()
     {
+        var contact = new Contact("Armen",32);
+        Assert.Equal("Armen", contact.Name);
+        Assert.Equal(32,contact.Id);
 
     }
 }
